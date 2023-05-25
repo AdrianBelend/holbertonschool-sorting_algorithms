1e0a87c7730671b47c5abfea01e3760f9714a43b
ELCBAh <6211@holbertonstudents.com>
2023-05-25 18:40:39 -0300

    trying a different approach, seems to work but need debugging. Infinite loop

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,26 +8,21 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL, *prev = NULL, *temp = NULL;
+	listint_t *current = NULL, *temp = NULL;
 
 	if (list == NULL || *list == NULL)
 		return;
-	current = *list;
-	for (; current != NULL; current = current->next)
+	for (current = *list; current != NULL; current = current->next)
 	{
-		prev = current->prev;
-		if (prev && prev->prev && current->n < prev->n)
+		if (current->prev != NULL && current->n < current->prev->n)
 		{
-			temp = current->next;
-			current->next = prev;
-			prev->prev->next = current;
-			current->prev = prev->prev;
-			prev->prev = current;
-			prev->next = temp;
-			if (temp)
-				temp->prev = prev;
-			if (!current->prev)
-				*list = current;
+			temp = current;
+			if (current->next != NULL)
+				current->next = current->prev->next;
+			current->prev = current->prev->prev;
+			current->prev->next = temp->next;
+			if (current->next != NULL)
+				current->next->prev = temp->prev;
 			print_list(*list);
 		}
 	}
