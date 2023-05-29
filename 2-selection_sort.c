ab8234c6514711836b1be4f8119d57b1c22638a3
ELCBAh <6211@holbertonstudents.com>
2023-05-28 21:15:39 -0300

    selection sort function added and properly working

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * selection_sort - initializes prototype
+ * @array: array to sort
+ * @size: soze of the array to sort
+ * Description: sort array in ascending order
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
