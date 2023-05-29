50c3629572872c446884c33aef1dfffee6029e53
ELCBAh <6211@holbertonstudents.com>
2023-05-28 21:21:55 -0300

    quick sort added, lacking functionality

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * quick_sort - initialize protype
+ * @array: array to be sorted
+ * @size: size of array
+ * Description: sort array of integers in ascending order
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+}
